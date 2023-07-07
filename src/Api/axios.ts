d0e1bedea8467fdfc04d83bb16ff4fefa9a76224
Allan <allancunha20@hotmail.com>
2023-07-06 21:12:35 -0300

    refactor: Axios

--- src/Api/axios.ts
+++ src/Api/axios.ts
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const API_URL = axios.create({
 
-    baseURL: "http://localhost:8080",
+    baseURL: "http://crud-cardapio-java-production.up.railway.app",
     timeout: 5000,
 
 })
 
-export { API_URL }
+export { API_URL };
+
